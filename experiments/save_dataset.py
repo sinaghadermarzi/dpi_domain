8aba0d146be6883700706c5e8d1bb09e2d11743e
sinaghadermarzi <sina.ghadermarzi@gmail.com>
2022-01-18 17:52:17 -0500

    added only with ki when saving dataset

--- experiments/save_dataset.py
+++ experiments/save_dataset.py
@@ -13,7 +13,7 @@ def hash_pairs(pair):
     pair_str =pid+cid
     return hashlib.md5(pair_str.encode(encoding='UTF-8')).hexdigest()
 
-def save_dataset_csv(input_pairs,bindingdb_recs, output_path):
+def save_dataset_csv(input_pairs,bindingdb_recs, output_path, only_with_ki=True):
     #this is to ensure a reproducible order of entries in the dataset while having shuffled them (prevent any hidden signal in the order of pairs)
     prot_cmp_pairs = sorted(input_pairs, key=hash_pairs)
     #prepare a csv file with inputs (SMILES strings for compounds and amino-acid sequences for proteins)
@@ -27,16 +27,20 @@ def save_dataset_csv(input_pairs,bindingdb_recs, output_path):
     SMILES = get_SMILES(cid_list)
     seq = get_uniprot_sequences(pid_list)
     with open (output_path, "w") as outf:
-        outf.writelines("protein_uniprot_id,compound_pubchem_cid,protein_sequence,comppound_SMILES,binary_interaction,affinity[-log10(Ki/10e9)]\n")
+        outf.writelines("protein_uniprot_id,compound_pubchem_cid,protein_sequence,compound_SMILES,binary_interaction,affinity[-log10(Ki/10e9)]\n")
         for (prot, compound) in prot_cmp_pairs:
             aff = bindingdb_recs.get_pAff_avg(prot, compound, "Ki")
             if type(aff)==str:
                 aff_str = "none"
             else:
                 aff_str= str(bindingdb_recs.get_pAff_avg(prot, compound,"Ki"))
-            bin_int = str(int(bindingdb_recs.get_bin_interaction((prot,compound))))
-            line = prot +"," +compound+ ","+seq[prot]+ ","+ SMILES[compound] + ","+bin_int+","+aff_str+"\n"
-            outf.writelines(line)
+                
+            if (aff_str=="none") and only_with_ki:
+                pass
+            else:
+                bin_int = str(int(bindingdb_recs.get_bin_interaction((prot,compound))))
+                line = prot +"," +compound+ ","+seq[prot]+ ","+ SMILES[compound] + ","+bin_int+","+aff_str+"\n"
+                outf.writelines(line)
 
 
 
